6096584b877bb26c01b8c064a55c11fc8012670b
Anton Reznikov <zoomfrost@gmail.com>
2026-04-15 22:02:41 +0300

    change backend to elisya

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,7 @@
-// import type { Core } from '@strapi/strapi';
+import { Elysia } from "elysia";
 
-export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
-};
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
